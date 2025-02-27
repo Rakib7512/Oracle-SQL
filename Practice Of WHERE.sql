dfb6c72cb927465d4abdfb937207b18c36db28b8
Rakib7512 <mdrakibislam7512@gmail.com>
2025-02-27 18:34:36 +0600

    27-02-25
    27-02-25

--- Practice Of WHERE.sql
+++ Practice Of WHERE.sql
@@ -21,8 +21,52 @@ where hire_date in('17-JUN-03','14-JAN-07');
 
 
 
-select first_name,salary,department_id, hire_date
+
+
+
+select last_name,salary,department_id, hire_date
 from employees
-where hire_date in('17-JUN-03','14-JAN-07');
+where department_id=60
+or department_id=80
+and salary > 10000
+order by last_name desc, hire_date;
+
+
+
+
+select distinct salary
+from employees
+order by salary desc
+OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY;
+
+
+select distinct salary
+from employees
+order by salary desc
+FETCH first 5 ROWS ONLY;
+
+
+
+
+select last_name
+ from employees
+
+where substr(last_name,2,2)='an';
+
+
+
+
+
+select last_name
+ from employees
+
+where substr(last_name,-3)='aan';
+
+
+
+
+
+
+
 
 
