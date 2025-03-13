a7189331cc5ff2fcbf23d6ebb8a67aee266d9376
Rakib7512 <mdrakibislam7512@gmail.com>
2025-03-13 16:35:51 +0600

    13-02
    13-02

--- OR ANY ALL quary.sql
+++ OR ANY ALL quary.sql
@@ -77,11 +77,7 @@ FROM EMPLOYEES
  (SELECT JOB_ID FROM EMPLOYEES WHERE EMPLOYEE_ID IN
         (SELECT EMPLOYEE_ID FROM JOB_HISTORY WHERE JOB_ID='IT_PROG'))
         
-        
-        
-        
-
-        select department_id,job_id
+select department_id,job_id
         from employees
         where employee_id in(select employee_id
         from job_history
