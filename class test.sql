eeb6a835bfd63eb129cfa140550540d07ac0f09b
Rakib7512 <mdrakibislam7512@gmail.com>
2025-03-19 16:25:40 +0600

    add
    add

--- class test.sql
+++ class test.sql
@@ -1,6 +1,6 @@
 
 --QUESTION NO.1
-CREATE TABLE emp(
+CREATE TABLE emp1287675(
     eid NUMBER(10) CONSTRAINT emp_eid_pk PRIMARY KEY,
     ename VARCHAR2(30) NOT NULL,
     salary NUMBER(10) NOT NULL,
@@ -8,56 +8,51 @@ CREATE TABLE emp(
     address VARCHAR2(50)
 );
 SELECT
-    * FROM emp;
+    * FROM emp1287675;
 --QUESTION NO.2
-ALTER TABLE emp
+ALTER TABLE emp1287675
     ADD(
     department_id NUMBER(10),
     dept_name VARCHAR2(20)
 );
-select *from emp;
+select *from emp1287675;
 --QUESTION NO.3
-INSERT INTO emp VALUES(1,'Rakib',80000,'01-Jan-20','Dhaka',80,'JEE');
-INSERT INTO emp VALUES(2,'Eias prodhan',90000,'01-Jan-21','Dhaka',90,'JEE');
-INSERT INTO emp VALUES(3,'Parvej Hossain',60000,'01-Jan-22','Madaripur',80,'JEE');
-INSERT INTO emp VALUES(4,'Rohim Khan',40000,'01-Jan-23','Munshiganj',80,'JEE');
-INSERT INTO emp VALUES(5,'Atik',70000,'01-Jan-24','Dhaka',90,'JEE');
+INSERT INTO emp1287675 VALUES(1,'Rakib',80000,'01-Jan-20','Dhaka',80,'JEE');
+INSERT INTO emp1287675 VALUES(2,'Eias prodhan',90000,'01-Jan-21','Dhaka',90,'JEE');
+INSERT INTO emp1287675 VALUES(3,'Parvej Hossain',60000,'01-Jan-22','Madaripur',80,'JEE');
+INSERT INTO emp1287675 VALUES(4,'Rohim Khan',40000,'01-Jan-23','Munshiganj',80,'JEE');
+INSERT INTO emp1287675 VALUES(5,'Atik',70000,'01-Jan-24','Dhaka',90,'JEE');
 
 --QUESTION NO.4
-CREATE VIEW emp_view 
+CREATE VIEW emp1287675_view 
 AS SELECT ENAME, DEPT_NAME, salary
-FROM emp
+FROM emp1287675
 WHERE department_id = 80;
 
-SELECT * FROM emp_view;
+SELECT * FROM emp1287675_view;
 
 
 
 
 --QUESTION NO.5
-CREATE SEQUENCE emp_eid_seq
+CREATE SEQUENCE emp1287675_eid_seq
         START WITH 6
         INCREMENT BY 1
         MAXVALUE 9999
         NOCACHE
         NOCYCLE;
         
-        
-SELECT emp_eid_seq.NEXTVAL
-FROM DUAL;
-
-
-
-SELECT
-    * FROM emp;
-INSERT INTO emp VALUES (emp_eid_seq.nextval,'Abdullah',80000,'01-JAN-08','sangkor','90','NT');
 
 --QUESTION NO.6
-INSERT INTO emp(eid,ename,salary,hire_date,address,department_id,dept_name)
-SELECT emp_eid_seq.nextval,first_name||' '||last_name,salary,hire_date,country_name,department_id,department_name
+INSERT INTO emp1287675(eid,ename,salary,hire_date,address,department_id,dept_name)
+SELECT emp1287675_eid_seq.nextval,first_name||' '||last_name,salary,hire_date,country_name,department_id,department_name
 FROM employees NATURAL JOIN departments NATURAL JOIN locations NATURAL JOIN countries
 WHERE department_id IN(select department_id
                         from employees
                             where salary<4000);
 
-SELECT * FROM emp;
+SELECT * FROM emp1287675;
+
+DROP TABLE emp1287675;
+drop VIEW  emp1287675_view;
+DROP SEQUENCE emp1287675_eid_seq;
